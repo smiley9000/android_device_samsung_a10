8fa3bc851b7c7561e234ba4ccefbb63623efec0b
Smiley <59495159+smiley9000@users.noreply.github.com>
2023-08-13 13:18:06 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,5 +6,6 @@ PRODUCT_PACKAGES += \
 	android.hardware.fastboot@1.0-impl-mock \
 	android.hardware.fastboot@1.0-impl-mock.recovery
 
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.fastbootd.available=true
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
